0e6692871c977ade2145b1759a4da9a853f6f219
bianca-rodrigues <biancapereirarodrigues02@gmail.com>
2022-12-13 17:45:59 -0300

    add service

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/service/usuario.service';
+import { Usuario } from '../usuario.model';
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit{
 
+  listarUsuario: Usuario[] = [];
+
+  constructor(private service: UsuarioService){ }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listarUsuario) => { this.listarUsuario = listarUsuario;}
+  }
 }
--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Usuario } from '../components/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  private readonly API = "https://jsonplaceholder.typicode.com/users";
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Usuario[]>{
+    return this.http.get<Usuario[]>(this.API);
+  }
 }
